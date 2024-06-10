14714a8959cba8168c0e1a0e2405233f7050d7bb
Łukasz <133879255+lkrzyzewski@users.noreply.github.com>
2024-06-10 11:59:08 +0200

    Update fields.py
    Date fixes

--- ldapdb/models/fields.py
+++ ldapdb/models/fields.py
@@ -364,15 +364,15 @@ DateTimeField.register_lookup(GteLookup)
 DateTimeField.register_lookup(InLookup)
 
 
-EPOCH = timezone.utc.localize(datetime.datetime.utcfromtimestamp(0))
+#EPOCH = timezone.utc.localize(datetime.datetime.utcfromtimestamp(0))
 
 
 def datetime_from_timestamp(ts):
-    return timezone.utc.localize(datetime.datetime.utcfromtimestamp(ts))
+    return datetime.datetime.fromtimestamp(ts)
 
 
 def timestamp_from_datetime(dt):
-    return int((timezone.utc.normalize(dt) - EPOCH).total_seconds())
+    return dt.timestamp()
 
 
 class TimestampField(LdapFieldMixin, fields.DateTimeField):
